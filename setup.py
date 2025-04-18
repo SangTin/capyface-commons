e725132583472227f20b9da69c5904fc851dc39c
Sang Nguyen <sang080304@gmail.com>
2025-04-18 03:52:34 +0000

    update

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
+        "requests>=2.25.0",
     ],
     author="CapyFace Team",
     author_email="sang080304@gmail.com",
