e134f500e9cbb51c44042f7a354e3925a7280576
Sang Nguyen <sang080304@gmail.com>
2025-04-20 12:56:07 +0000

    fix import

---
+++ capyface_commons/grpc_service/__init__.py
@@ -0,0 +1,4 @@
+from .service_registry import service_registry, RedisServiceRegistry
+from .service_gateway import service_gateway, ServiceGateway
+
+__all__ = ['service_registry', 'RedisServiceRegistry', 'service_gateway', 'ServiceGateway']
