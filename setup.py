77c9dd532e7e99926dab64adf450a773fd41eda8
SangTin <sang080304@gmail.com>
2025-05-25 11:31:32 +0000

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="capyface-commons",
-    version="0.4.0",
+    version="0.5.0",
     packages=find_packages(),
     package_data={
         '': ['*.proto'],
